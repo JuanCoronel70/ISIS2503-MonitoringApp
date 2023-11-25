d4575817e330c09487bd242653b63a06bc404e51
JuanCoronel70 <juanescoro2010@gmail.com>
2023-11-25 11:25:56 -0500

    a

--- measurements/forms.py
+++ measurements/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Measurement
+from variables.models import Variable
+from places.models import Place
 
 class MeasurementForm(forms.ModelForm):
     class Meta:
@@ -19,3 +21,15 @@ class MeasurementForm(forms.ModelForm):
             'place' : 'Place',
             #'dateTime' : 'Date Time',
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        variable = cleaned_data.get('variable')
+        place = cleaned_data.get('place')
+
+        # Realizamos la validación de la existencia de la ForeignKey aquí
+        if variable and not Variable.objects.filter(pk=variable.pk).exists():
+            self.add_error('variable', forms.ValidationError('La variable no existe.'))
+
+        if place and not Place.objects.filter(pk=place.pk).exists():
+            self.add_error('place', forms.ValidationError('El lugar no existe.'))
--- measurements/models.py
+++ measurements/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from variables.models import Variable
+from places.models import Place
 
 class Measurement(models.Model):
     variable = models.ForeignKey(Variable, on_delete=models.CASCADE, default=None)
     value = models.FloatField(null=True, blank=True, default=None)
     unit = models.CharField(max_length=50)
-    place = models.CharField(max_length=50)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, default=None)
     dateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
